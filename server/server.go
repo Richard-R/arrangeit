3ab5769b458ea4ed573d5fb70250bb9cf7327724
Richard Ruan <richardruan@berkeley.edu>
2020-06-17 15:55:08 -0700

    testing push

---
+++ server/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"arrangeit/controllers"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusCreated, "Theoretical home page")
+	})
+
+	e.GET("/apple", func(c echo.Context) error {
+		return c.String(http.StatusCreated, "App won't go to different web pages right? Why use this?")
+	})
+
+	e.GET("/users/:id", controllers.GetUser)
+	//e.GET("/users/:id/:name", controllers.testTwo)
+
+	e.Logger.Fatal(e.Start(":8081"))
+
+}
